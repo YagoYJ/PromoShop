e5be0f02c11b8b90dcdf4a3ba708b9ae2067679c
Yago Juan <48865087+YagoYJ@users.noreply.github.com>
2020-12-12 21:28:53 -0300

    Create the promotional card component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import PromotionCard from "./components/promotion/Card";
+import "./global.css";
 
-function App() {
+export default function App() {
+  const promotion = {
+    id: 1,
+    title: "Par de Alto Falante Magnum 6 polegadas Rex Fusion 800W 4 Ohms",
+    price: 99.99,
+    imageUrl:
+      "https://images.tcdn.com.br/img/img_prod/710062/par_de_alto_falante_magnum_6_rex_fusion_200w_4_ohms_1623_1_20200828134205.png",
+    url:
+      "https://www.artsomonline.com.br/alto-falantes/alto-falantes/6/par-de-alto-falante-magnum-6-rex-fusion-200w-4-ohms",
+    comments: [
+      {
+        id: 1,
+        comment: "Top d+",
+      },
+      {
+        id: 1,
+        comment: "Top d+",
+      },
+    ],
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="App"
+      style={{
+        maxWidth: 800,
+        margin: "30px auto",
+      }}
+    >
+      <PromotionCard promotion={promotion} />
     </div>
   );
 }
-
-export default App;
---
+++ src/components/promotion/Card/index.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./styles.css";
+
+export default function PromotionCard({ promotion }) {
+  return (
+    <div className="promotionCard">
+      <div className="productImage">
+        <img src={promotion.imageUrl} alt={promotion.title} />
+      </div>
+
+      <div className="descriptions">
+        <h1>{promotion.title}</h1>
+        <span>R$ {promotion.price}</span>
+
+        <footer>
+          {promotion.comments.length > 0 && (
+            <p className="lastComment">"{promotion.comments[0].comment}"</p>
+          )}
+          <p className="commentsCount">
+            {promotion.comments.length}
+            {promotion.comments.length > 1 ? " comentários" : " comentário"}
+          </p>
+          <a href={promotion.url} target="_blank" rel="noreferrer">
+            IR PARA O SITE
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
+}
